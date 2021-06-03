7342eb66d02a9eacdf489c8bb81b32a76c786b1d
Vivi Tran <vihanhtranjobb@hotmail.com>
2021-06-03 11:28:15 +0200

    Updates

---
+++ precache-manifest.f9d9920721a49c419e818fac6c417179.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e99e0e18b1652176f69dbd474cceac",
+    "url": "/CVpage/index.html"
+  },
+  {
+    "revision": "f9293f1fde6ae015f7ca",
+    "url": "/CVpage/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "2a01ca5ba8170d5b5fd1",
+    "url": "/CVpage/static/css/main.ec000262.chunk.css"
+  },
+  {
+    "revision": "f9293f1fde6ae015f7ca",
+    "url": "/CVpage/static/js/2.1133e450.chunk.js"
+  },
+  {
+    "revision": "dac798c0307ffb3e2151e024021fbb85",
+    "url": "/CVpage/static/js/2.1133e450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a01ca5ba8170d5b5fd1",
+    "url": "/CVpage/static/js/main.3a1228b1.chunk.js"
+  },
+  {
+    "revision": "dbdf71f3e11511e23797",
+    "url": "/CVpage/static/js/runtime-main.5b6ab4a0.js"
+  },
+  {
+    "revision": "d9ddbb9239ad6e1e69dbc5708431e113",
+    "url": "/CVpage/static/media/flower-break.d9ddbb92.png"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "/CVpage/static/media/github.e45ebd52.png"
+  },
+  {
+    "revision": "4274e6e1f71322d8ae04a7496497be28",
+    "url": "/CVpage/static/media/letter.4274e6e1.png"
+  },
+  {
+    "revision": "ed15672fbd6eaf9c4c802dd7cefd6bc0",
+    "url": "/CVpage/static/media/linkedin.ed15672f.png"
+  },
+  {
+    "revision": "966a4aaaaedcff7f8aaf91eaae02e193",
+    "url": "/CVpage/static/media/profile-pic.966a4aaa.png"
+  }
+]);
